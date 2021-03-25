98276c70611b69d3192698daad70c8729aac7ed0
Matheus Lúcio <matheusluciox@gmail.com>
2021-03-25 00:11:37 -0300

    ajustes heroku

--- quiz/settings.py
+++ quiz/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
