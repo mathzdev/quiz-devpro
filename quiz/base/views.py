78d9c949759d74d257c734ef276c05529ad7d9f7
Matheus Lúcio <matheusluciox@gmail.com>
2021-03-25 21:25:40 -0300

    ajustes pro heroku

--- quiz/base/views.py
+++ quiz/base/views.py
@@ -74,19 +74,27 @@ def classificacao(requisicao):
     except KeyError:
         return redirect('/')
     else:
+
         pontos_dct = Resposta.objects.filter(aluno_id=aluno_id).aggregate(Sum('pontos'))
         pontuacao_do_aluno = pontos_dct['pontos__sum']
 
-        numero_de_alunos_com_maior_pontuaca = Resposta.objects.values('aluno').annotate(Sum('pontos')).filter(
+        if pontuacao_do_aluno is None:
+            pontuacao_do_aluno = 0
+
+        numero_de_alunos_com_maior_pontuacao = Resposta.objects.values('aluno').annotate(Sum('pontos')).filter(
             pontos__sum__gt=pontuacao_do_aluno).count()
+
+        if numero_de_alunos_com_maior_pontuacao is None:
+            numero_de_alunos_com_maior_pontuacao = 0
 
         primeros_alunos_da_classificacao = list(Resposta.objects.values('aluno', 'aluno__nome').annotate(
             Sum('pontos')).order_by('-pontos__sum')[:5])
 
         context = {
             'pontuacao_do_aluno': pontuacao_do_aluno,
-            'posicao_do_aluno': numero_de_alunos_com_maior_pontuaca + 1,
-            'primeros_alunos_da_classificacao': primeros_alunos_da_classificacao
+            'posicao_do_aluno': numero_de_alunos_com_maior_pontuacao + 1,
+            'primeros_alunos_da_classificacao': primeros_alunos_da_classificacao,
+            'length_alunos': len(primeros_alunos_da_classificacao)
         }
 
         return render(requisicao, 'base/classificacao.html', context)
